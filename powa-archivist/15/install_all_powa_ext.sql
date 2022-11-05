45773de9fd3bc7eff9db2af83a673ff462c7e9da
Julien Rouhaud <julien.rouhaud@free.fr>
2022-11-05 20:31:23 +0800

    powa-archivist: add support for pg15

---
+++ powa-archivist/15/install_all_powa_ext.sql
@@ -0,0 +1,12 @@
+CREATE EXTENSION hypopg;
+CREATE database powa;
+\c powa
+CREATE EXTENSION pg_stat_statements;
+CREATE EXTENSION btree_gist;
+CREATE EXTENSION pg_qualstats;
+CREATE EXTENSION pg_stat_kcache;
+CREATE EXTENSION pg_track_settings;
+CREATE EXTENSION pg_wait_sampling;
+CREATE EXTENSION powa;
+\c template1
+CREATE EXTENSION hypopg;
